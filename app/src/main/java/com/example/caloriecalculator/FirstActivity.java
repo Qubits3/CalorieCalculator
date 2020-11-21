df8b62c3fe32f1367fefd6092fb6128f668f0ae5
murat <muratipekk033@gmail.com>
2020-11-21 15:39:06 +0300

    profil butonu eklendi ve diğer cihazlarla uyumluluğu artırıldı

--- app/src/main/java/com/example/caloriecalculator/FirstActivity.java
+++ app/src/main/java/com/example/caloriecalculator/FirstActivity.java
@@ -14,8 +14,6 @@ public class FirstActivity extends AppCompatActivity {
         setContentView(R.layout.activity_first);
         setTitle("Giriş Sayfası");
 
-        Intent walk = new Intent(getApplicationContext(), MapsActivity.class);
-        startActivity(walk);
     }
     public void onaylaButonu(View view){
         Intent intent = new Intent(FirstActivity.this, MainActivity.class);
