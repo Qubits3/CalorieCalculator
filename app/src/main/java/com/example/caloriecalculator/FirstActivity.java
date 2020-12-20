aa4fca8e27cff8c05b6c60fe9cb019765fbda2b6
hsynaktepe <56879020+hsynaktepe@users.noreply.github.com>
2020-12-21 02:08:22 +0300

    -Added image changing with gender radiobutton and still need improvements.

--- app/src/main/java/com/example/caloriecalculator/FirstActivity.java
+++ app/src/main/java/com/example/caloriecalculator/FirstActivity.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
@@ -22,6 +23,7 @@ public class FirstActivity extends AppCompatActivity {
     EditText weightL;
     private RadioButton cinsiyetRB;
     private RadioGroup cinsiyetRG;
+    ImageView img;
 
 
     @Override
@@ -38,7 +40,44 @@ public class FirstActivity extends AppCompatActivity {
         heightL = findViewById(R.id.heightLabel);
         weightL = findViewById(R.id.weightLabel);
         cinsiyetRG = findViewById(R.id.radio);
+        img = findViewById(R.id.mainimg);
 
+        //prefte kayıtlı resim varsa burada çekilip gösterilecek(yapıalcak)
+        img.setImageResource(R.drawable.notselected);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        cinsiyetRG.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+
+                RadioButton rb = findViewById(checkedId);
+                int index = cinsiyetRG.indexOfChild(rb);
+
+                switch (index){
+
+                    case 1:
+                        img.setImageResource(R.drawable.male);
+                       // Toast.makeText(getApplicationContext(), rb.getText(), Toast.LENGTH_SHORT).show();
+                        break;
+
+                    case 2:
+                        img.setImageResource(R.drawable.female);
+                        //Toast.makeText(getApplicationContext(), rb.getText(), Toast.LENGTH_SHORT).show();
+                        break;
+                    default:
+                        img.setImageResource(R.drawable.notselected);
+                      //  Toast.makeText(getApplicationContext(), "bum", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+                //Toast.makeText(getApplicationContext(), rb.getText(), Toast.LENGTH_SHORT).show();
+
+            }
+        });
 
     }
 
@@ -76,6 +115,8 @@ public class FirstActivity extends AppCompatActivity {
             editor.apply(); //Veriler sharedprefe aktarılır.
             Toast.makeText(this, "Bilgileriniz Başarıyla Kaydedildi!", Toast.LENGTH_LONG).show();
 
+            img.setImageResource(R.drawable.female);
+
             Intent intent = new Intent(FirstActivity.this, MainActivity.class);
             startActivity(intent);
         }
@@ -90,8 +131,7 @@ girilen verileri gösterme
 xml in başına görsel eklenecek
 bilgiler girildiğinde butona  tıklanıldığında bool ile true değeri verilecek ve bu sayfa ilk başta birdaha gösterilmeyecek
 
-?eğer yapılabilinirse cinsiyete göre resim değişmesi yapılacak
-
++eğer yapılabilinirse cinsiyete göre resim değişmesi yapılacak
 +karakter sınırı ve özel karakter girilmemesi
 +boş olursa hata mesajı verdirmesi
 +cinsiyet seçimi sharede aktarılacak
