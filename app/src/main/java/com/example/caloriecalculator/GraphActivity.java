c58238376f17d4c12c12c13ae6a92dfa4901d041
Murat03 <muratipekk033@gmail.com>
2021-01-05 14:38:00 +0300

    Grafik sekmesi tamamlandi bir hata var ve verim yok

--- app/src/main/java/com/example/caloriecalculator/GraphActivity.java
+++ app/src/main/java/com/example/caloriecalculator/GraphActivity.java
@@ -20,9 +20,14 @@ public class GraphActivity extends AppCompatActivity {
     ImageView saturdaypic;
     ImageView sundaypic;
     SharedPreferences sharedPreferences;
-    int day;
     int height;
-    String dailyCal;
+    String mondayCal;
+    String tuesdayCal;
+    String wednesdayCal;
+    String thursdayCal;
+    String fridayCal;
+    String saturdayCal;
+    String sundayCal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,20 +45,52 @@ public class GraphActivity extends AppCompatActivity {
         saturdaypic = findViewById(R.id.saturdaypic);
         sundaypic = findViewById(R.id.sundaypic);
 
-        dailyCal = sharedPreferences.getString("dailyCalorieOfAllDay","");
+        mondayCal = sharedPreferences.getString("2","");
+        tuesdayCal = sharedPreferences.getString("3","");
+        wednesdayCal = sharedPreferences.getString("4","");
+        thursdayCal = sharedPreferences.getString("5","");
+        fridayCal = sharedPreferences.getString("6","");
+        saturdayCal = sharedPreferences.getString("7","");
+        sundayCal = sharedPreferences.getString("1","");
 
-        day = sharedPreferences.getInt("day",1);
 
-        String bironcekigun = sharedPreferences.getString(String.valueOf(day - 1), "");
+        //day = sharedPreferences.getInt("day",1);
 
-        if (!dailyCal.equals("0")){
-            height = Integer.parseInt(dailyCal.substring(0, dailyCal.indexOf(".")));
+        //String bironcekigun = sharedPreferences.getString(String.valueOf(day - 1), "");
+
+        if (!mondayCal.equals("0") && !mondayCal.equals("")){
+            height = Integer.parseInt(mondayCal.substring(0, mondayCal.indexOf(".")));
             height /= 5;
+            mondaypic.getLayoutParams().height = height;
+        }if (!tuesdayCal.equals("0") && !tuesdayCal.equals("")){
+            height = Integer.parseInt(tuesdayCal.substring(0, tuesdayCal.indexOf(".")));
+            height /= 5;
+            tuesdaypic.getLayoutParams().height = height;
+        }if (!wednesdayCal.equals("0") && !wednesdayCal.equals("")){
+            height = Integer.parseInt(wednesdayCal.substring(0, wednesdayCal.indexOf(".")));
+            height /= 5;
+            wednesdaypic.getLayoutParams().height = height;
+        }if (!thursdayCal.equals("0") && !thursdayCal.equals("")){
+            height = Integer.parseInt(thursdayCal.substring(0, thursdayCal.indexOf(".")));
+            height /= 5;
+            thursdaypic.getLayoutParams().height = height;
+        }if (!fridayCal.equals("0") && !fridayCal.equals("")){
+            height = Integer.parseInt(fridayCal.substring(0, fridayCal.indexOf(".")));
+            height /= 5;
+            fridaypic.getLayoutParams().height = height;
+        }if (!saturdayCal.equals("0") && !saturdayCal.equals("")){
+            height = Integer.parseInt(saturdayCal.substring(0, saturdayCal.indexOf(".")));
+            height /= 5;
+            saturdaypic.getLayoutParams().height = height;
+        }if (!sundayCal.equals("0") && !sundayCal.equals("")){
+            height = Integer.parseInt(sundayCal.substring(0, sundayCal.indexOf(".")));
+            height /= 5;
+            sundaypic.getLayoutParams().height = height;
         }
 
-        daytograph();
-    }
 
+    }
+/*
         public void daytograph(){
         if(day == 2){
             mondaypic.getLayoutParams().height = height;
@@ -73,5 +110,5 @@ public class GraphActivity extends AppCompatActivity {
 
         }
     }
-
+*/
 }
